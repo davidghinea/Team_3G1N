e3cea5abd74dd0203fa74dd93e004d8461d9d765
David Ghinea <david.ghinea01@gmail.com>
2025-11-01 15:53:43 +0200

    Revert "added login page"
    This reverts commit 579d8a51f8b4c9a6d6e6e7f5c3a3cc8d190f85ce.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { IdeasSidebarShadcn } from "@/components/personal-components/ideas-sidebar-shadcn";
-import { AuthProvider } from "@/contexts/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <IdeasSidebarShadcn>
+        {children}
+        </IdeasSidebarShadcn>
       </body>
     </html>
   );
